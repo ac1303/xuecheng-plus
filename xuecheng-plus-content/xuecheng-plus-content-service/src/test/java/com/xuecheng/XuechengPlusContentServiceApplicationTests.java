56ce5a8d7d9cc0ddd9b242278c8e8d3096406826
ac1303 <878375551@qq.com>
2023-01-17 14:30:20 +0800

    课程查询DAO，Service层开发和调试

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package com.xuecheng;
 
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XuechengPlusContentServiceApplicationTests {
 
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @Test
-    void contextLoads() {
+    void testCourseBaseMapper() {
+        CourseBase courseBase = courseBaseMapper.selectById("22");
+        Assertions.assertNotNull(courseBase);
+    }
+    @Test
+    void testCourseBaseInfoService() {
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(
+                new PageParams(1, 10),
+                new QueryCourseParamsDto());
+        System.out.println(courseBasePageResult);
     }
 
 }
