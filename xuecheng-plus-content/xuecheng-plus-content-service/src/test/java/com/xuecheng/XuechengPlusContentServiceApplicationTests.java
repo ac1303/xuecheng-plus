08cfd501b8bfce48c7fbf0d54b0a3091c820e48d
ac1303 <878375551@qq.com>
2023-01-19 16:49:52 +0800

    配置nacos，创建网关模块

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/XuechengPlusGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusGatewayApplication.class, args);
+    }
+
+}
