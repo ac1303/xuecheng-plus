6faf190ed8dff99ce81db3349b3e9290f3c2072f
ac1303 <878375551@qq.com>
2023-01-23 15:56:10 +0800

    视频上传和文件预览功能完成

---
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/BigFilesController.java
@@ -0,0 +1,74 @@
+package com.xuecheng.media.api;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.xuecheng.base.execption.XueChengPlusException;
+import com.xuecheng.base.model.RestResponse;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.po.MediaFiles;
+import com.xuecheng.media.service.MediaFileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author 小王的饭饭
+ * @create 2023/1/23 10:30
+ */
+@Api(value = "大文件上传接口", tags = "大文件上传接口")
+@RestController
+public class BigFilesController {
+
+    @Autowired
+    MediaFileService mediaFileService;
+
+    @ApiOperation(value = "文件上传前检查文件")
+    @PostMapping("/upload/checkfile")
+    public RestResponse<Boolean> checkfile(
+            @RequestParam("fileMd5") String fileMd5
+    ) throws Exception {
+        return mediaFileService.checkFile(fileMd5);
+    }
+
+    @ApiOperation(value = "分块文件上传前的检测")
+    @PostMapping("/upload/checkchunk")
+    public RestResponse<Boolean> checkchunk(@RequestParam("fileMd5") String fileMd5,
+                                            @RequestParam("chunk") int chunk) throws Exception {
+        return mediaFileService.checkChunk(fileMd5,chunk);
+    }
+
+    @ApiOperation(value = "上传分块文件")
+    @PostMapping("/upload/uploadchunk")
+    public RestResponse uploadchunk(@RequestParam("file") MultipartFile file,
+                                    @RequestParam("fileMd5") String fileMd5,
+                                    @RequestParam("chunk") int chunk) throws Exception {
+
+        return mediaFileService.uploadChunk(fileMd5,chunk,file.getBytes());
+    }
+
+    @ApiOperation(value = "合并文件")
+    @PostMapping("/upload/mergechunks")
+    public RestResponse mergechunks(@RequestParam("fileMd5") String fileMd5,
+                                    @RequestParam("fileName") String fileName,
+                                    @RequestParam("chunkTotal") int chunkTotal) throws Exception {
+        Long companyId = 1232141425L;
+
+        UploadFileParamsDto uploadFileParamsDto = new UploadFileParamsDto();
+        uploadFileParamsDto.setFileType("001002");
+        uploadFileParamsDto.setTags("课程视频");
+        uploadFileParamsDto.setRemark("");
+        uploadFileParamsDto.setFilename(fileName);
+        ContentInfo extensionMatch = ContentInfoUtil.findExtensionMatch(fileName);
+        String mimeType = extensionMatch.getMimeType();
+        uploadFileParamsDto.setContentType(mimeType);
+
+        return mediaFileService.mergechunks(companyId,fileMd5,chunkTotal,uploadFileParamsDto);
+
+    }
+
+
+}
